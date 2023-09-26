5f09ae6461138e142ca44e97b8bbc7f8f0e12bb7
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-09-26 10:46:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+destructivelyAppendCat('Ralph');
+console.log(cats);
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+destructivelyPrependCat('Bob');
+console.log(cats);
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+destructivelyRemoveLastCat();
+console.log(cats);
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+destructivelyRemoveFirstCat();
+console.log(cats);
+
+
+function appendCat(name) {
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+
+appendCat('Broom');
+console.log(cats);
+
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+console.log(prependCat('Arnold'));
+console.log(cats);
+
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+removeLastCat();
+console.log(cats);
+
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+removeFirstCat();
+console.log(cats);
